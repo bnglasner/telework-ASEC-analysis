5ea32c54bdca1c3a0896364211354c571c3d143d
bnglasner <54865110+bnglasner@users.noreply.github.com>
2023-11-02 13:18:16 -0400

    Update Data load and merge.R

--- Code/Data load and merge.R
+++ Code/Data load and merge.R
@@ -10,6 +10,7 @@ library(dplyr)
 library(readxl)
 library(readr)
 library(ipumsr)
+library(janitor)
 
 #################
 ### Set paths ###
@@ -43,8 +44,45 @@ setwd(path_data)
 #################
 
 telework_microdata_CPS <- read_csv("telework_microdata_CPS.csv")
-cps_monthly_files <- read_csv("cps_monthly_files.csv")
+telework_microdata_CPS <- telework_microdata_CPS %>% clean_names()
 
-ddi <- read_ipums_ddi("cps_00024.xml")
+cps_monthly_files <- read_csv("cps_monthly_files.csv")
+cps_monthly_files <- cps_monthly_files %>% 
+  clean_names()  %>%
+  select(serial, pernum, year, month, hrhhid ,hrhhid2, pulineno )
+
+ddi <- read_ipums_ddi("cps_00025.xml")
 data <- read_ipums_micro(ddi)
+data <- data %>% 
+  clean_names() %>% 
+  # filter(year >=2023) %>%
+  select(-serial, -pernum) %>%
+  mutate(hrhhid = as.character(hrhhid)) %>%
+  rename(pulineno = lineno)
+
+##################
+### Data merge ###
+##################
+
+merged_data <- inner_join(cps_monthly_files, data) # merge ipums with census using hhrid hrhhid2 and pulineno (lineno)
+merged_data <- inner_join(merged_data, telework_microdata_CPS) # merge census to telework using year month serial pernum
+
+#########################
+### Describe the data ###
+#########################
+
+table(merged_data$year, merged_data$month) # confirm that everything is March, 2023
+
+merged_data %>%
+  group_by(year, month) %>%
+  summarise(workers = sum(earnwt),
+            workers_diffany = sum(earnwt[diffany == 2])/workers,
+            workers_diffcare = sum(earnwt[diffcare == 2])/workers,
+            workers_diffhear = sum(earnwt[diffhear == 2])/workers,
+            workers_diffeye = sum(earnwt[diffeye == 2])/workers,
+            workers_diffrem = sum(earnwt[diffrem == 2])/workers,
+            workers_diffphys = sum(earnwt[diffphys == 2])/workers,
+            workers_diffmob = sum(earnwt[diffmob == 2])/workers
+  )
+
 
