546b3a1ad9e1a8fd08e8753ea8f140bcd145b33f
bnglasner <54865110+bnglasner@users.noreply.github.com>
2023-11-02 16:37:11 -0400

    Create CPS Disabled Workers and Remote Work.R

---
+++ Code/CPS Disabled Workers and Remote Work.R
@@ -0,0 +1,117 @@
+# Economic Innovation Group
+# Telework Analysis
+# 11/2/2023
+
+##########################
+#         Set Up         #  
+##########################
+
+library(dplyr)
+library(readxl)
+library(ggplot2)
+library(ipumsr)
+library(fixest)
+# https://cran.r-project.org/web/packages/fixest/vignettes/fixest_walkthrough.html#13_Other_standard-errors
+
+#################
+### Set paths ###
+#################
+
+# Define user-specific root folders
+root_folders <- c("bglasner" = "C:/Users/bglasner/Dropbox/GitHub",
+                  "bngla" = "C:/Users/bngla/Dropbox/GitHub",
+                  "Benjamin Glasner" = "C:/Users/Benjamin Glasner/Dropbox/GitHub")
+
+# Get current user
+current_user <- Sys.info()[["user"]]
+
+# Check if current user's root folder is defined
+if (current_user %in% names(root_folders)) {
+  path_project <- root_folders[[current_user]]
+} else {
+  stop("Root folder for current user is not defined.")
+}
+
+# Define paths
+path_data <- file.path(path_project, "telework-ASEC-analysis", "Data")
+path_output <- file.path(path_project, "telework-ASEC-analysis", "Output")
+
+# Set working directory
+setwd(path_data)
+
+
+#################
+### Data Load ###
+#################
+load("CPS_telework.RData")
+
+#########################
+### Describe the data ###
+#########################
+
+merged_data <- merged_data %>%
+  mutate(date = as.Date(paste0("1-",month,"-",year),tryFormats = c("%d-%m-%Y")))
+
+merged_data %>%
+  group_by(date) %>%
+  summarise(count = n())
+##########################
+### Trends in the data ###
+##########################
+
+monthly_respondent_worforce <- merged_data %>%
+  group_by(date) %>%
+  summarise(workers = sum(wtfinl))
+
+ P <- merged_data %>%
+  mutate(telework_factor = if_else(telework_any==1 & ahrsworkt>telework_hours,"Some Telework",
+                                   if_else(telework_any==1 & ahrsworkt==telework_hours,"All Telework", "No Telework"))) %>%
+  group_by(date, telework_factor) %>%
+  summarise(workers_diffany = sum(wtfinl[diffany == 2]),
+            workers_diffcare = sum(wtfinl[diffcare == 2]),
+            workers_diffhear = sum(wtfinl[diffhear == 2]),
+            workers_diffeye = sum(wtfinl[diffeye == 2]),
+            workers_diffrem = sum(wtfinl[diffrem == 2]),
+            workers_diffphys = sum(wtfinl[diffphys == 2]),
+            workers_diffmob = sum(wtfinl[diffmob == 2])
+  ) %>%
+  left_join(monthly_respondent_worforce) %>%
+  mutate(workers_diffany = workers_diffany/workers) %>% 
+  ggplot(aes(group = telework_factor,
+             color = telework_factor,
+             x = date,
+             y = workers_diffany)) +
+  geom_line() + 
+  geom_point() + 
+  ylab("Percent of the Respondent Workforce") +
+  xlab("Date") + 
+  ggtitle("Workers with Disabilities") + 
+  scale_y_continuous(limits = c(0,0.045),
+                     labels = scales::percent_format()) + 
+  theme_bw() +
+  guides(color = guide_legend(title = ""))
+
+ setwd(path_output)
+png(filename = "Share of disabled workforce and telework.png",width =  850, height = 600)
+plot(P)
+dev.off()
+ 
+##########################
+### Regression         ###
+##########################
+merged_data <- merged_data %>%
+  mutate(telework_factor = if_else(telework_any==1 & ahrsworkt>telework_hours,"Some Telework",
+                                   if_else(telework_any==1 & ahrsworkt==telework_hours,"All Telework", "No Telework")),
+         fully_remote = as.integer(telework_factor == "All Telework"),
+         hybrid_remote = as.integer(telework_factor == "Some Telework"),
+         any_remote = as.integer(telework_any == 1),
+         diffany_bin = as.integer(diffany==2))
+
+
+fully_remote = feols(fully_remote ~ diffany_bin | occ + ind, subset(merged_data,wtfinl>0), weights = merged_data$wtfinl[merged_data$wtfinl>0])
+hybrid_remote = feols(hybrid_remote ~ diffany_bin | occ + ind, subset(merged_data,wtfinl>0), weights = merged_data$wtfinl[merged_data$wtfinl>0])
+any_remote = feols(any_remote ~ diffany_bin | occ + ind, subset(merged_data,wtfinl>0), weights = merged_data$wtfinl[merged_data$wtfinl>0])
+
+
+etable(fully_remote, hybrid_remote, any_remote,
+       vcov = "twoway", headers = c("Fully Remote", "Hybrid Remote", "Any Remote"))
