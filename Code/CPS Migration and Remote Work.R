d41c58292bff08ddcb75ff2c54541b4ad7a5ca38
bnglasner <54865110+bnglasner@users.noreply.github.com>
2023-11-03 16:10:48 -0400

    Update CPS Migration and Remote Work.R

--- Code/CPS Migration and Remote Work.R
+++ Code/CPS Migration and Remote Work.R
@@ -64,52 +64,96 @@ merged_data %>%
 ##########################
 telework_hh <- merged_data %>%
   mutate(full_remote = as.integer(telework_any==1 & ahrsworkt==telework_hours),
-         any_remote = as.integer(telework_any==1)) %>%
+         hybrid_remote = as.integer(telework_any==1 & ahrsworkt>telework_hours),
+         any_remote = as.integer(telework_any==1),
+         remote_hours = if_else(telework_hours>0, telework_hours, 0)) %>%
   group_by(serial) %>%
-  summarize(full_remote = mean(full_remote),
-            any_remote = mean(any_remote)) %>%
+  summarize(household_work_hours = sum(ahrsworkt),
+            household_remote_hours = sum(remote_hours),
+            num_full_remote = sum(full_remote),
+            num_hybrid = sum(hybrid_remote),
+            full_remote = mean(full_remote),
+            any_remote = mean(any_remote),
+            num_workers = n(),
+            share_hybrid = num_hybrid/num_workers,
+            share_full = num_full_remote/num_workers,
+            share_remote_hours = household_remote_hours/household_work_hours) %>%
   mutate(household_remote_type = case_when(
     full_remote == 1  ~ "Fully Remote Household",
     any_remote >0 & full_remote < 1 ~ "Partially Remote Household",
     any_remote ==0  ~ "Not a Remote Household",
     TRUE ~ "Missing"
   )) %>% 
-  select(serial,household_remote_type)
+  select(serial,household_remote_type,
+         num_workers,num_hybrid,num_full_remote,
+         share_hybrid,share_full,share_remote_hours)
 
 merged_data %>%
   left_join(telework_hh) %>%
   group_by(household_remote_type) %>%
-  summarise(moved = weighted.mean(moved,w = asecwt, na.rm = TRUE))  
+  summarise(moved_count = sum(moved),
+    moved = weighted.mean(moved,w = asecwt, na.rm = TRUE))  
  
 ###########################
 ### Regression Analysis ###
 ###########################
-
-merged_hh_data <- merged_data %>%
+analysis_data <- merged_data %>%
   left_join(telework_hh) %>%
-  filter(asecwth > 0) %>%
-  arrange(serial,pernum) %>%
-  group_by(serial) %>% 
-  slice(1)
-  
-merged_hh_data$household_remote_type <- factor(merged_hh_data$household_remote_type, levels = c("Not a Remote Household","Partially Remote Household","Fully Remote Household"))
-  
-# Convert moving variables to binary format within the merged_hh_data dataset
-binary_moving_vars <- c("household_remote_type")
+  filter(asecwt > 0) %>%
+  filter(age >= 22 & age <= 64) %>%
+  mutate(telework_factor = if_else(telework_any==1 & ahrsworkt>telework_hours,"Some Telework",
+                                   if_else(telework_any==1 & ahrsworkt==telework_hours,"All Telework", "No Telework")))
+
+analysis_data %>%
+  group_by(telework_factor) %>%
+  summarise(count = n())
+
+analysis_data %>%
+  group_by(age) %>%
+  summarise(count = n()) %>%
+  ggplot(aes(x = age,
+             y = count,
+             size = count)) + 
+  geom_point() + 
+  theme_bw()
+
+analysis_data %>%
+  # group_by(moved) %>%
+  # group_by(moved_in_county) %>%
+  group_by(moved_out_county) %>%
+  summarise(count = n())
+
+# analysis_data <- merged_data %>%
+#   left_join(telework_hh) %>%
+#   filter(asecwth > 0) %>%
+# mutate(telework_factor = if_else(telework_any==1 & ahrsworkt>telework_hours,"Some Telework",
+# if_else(telework_any==1 & ahrsworkt==telework_hours,"All Telework", "No Telework"))) %>%
+#   arrange(serial,pernum) %>%
+#   group_by(serial) %>%
+#   slice(1)
+
+analysis_data$household_remote_type <- factor(analysis_data$household_remote_type, levels = c("Not a Remote Household","Partially Remote Household","Fully Remote Household"))
+analysis_data$telework_factor <- factor(analysis_data$telework_factor, levels = c("No Telework","Some Telework","All Telework"))
+
+# Convert moving variables to binary format within the analysis_data dataset
+
+# binary_moving_vars <- c("household_remote_type + num_workers")
+binary_moving_vars <- c("telework_factor + num_workers")
 
 # Define the weights object for the feols
-asecwth <- merged_hh_data$asecwth
+# asecwth <- analysis_data$asecwth
+asecwth <- analysis_data$asecwt
 
 # Define a custom function to run feols with given dependent variable
 baseline_feols <- function(dependent_variable, independent_variable_bin) {
   feols(as.formula(paste(dependent_variable, "~", independent_variable_bin, "| occ2010 + ind1990")), 
-        data = merged_hh_data,
+        data = analysis_data,
         weights = asecwth[asecwth > 0])
 }
 
 ind_info_feols <- function(dependent_variable, independent_variable_bin) {
-  feols(as.formula(paste(dependent_variable, "~", independent_variable_bin, "| occ2010 + ind1990 + race + sex + marst + educ")), 
-        data = merged_hh_data,
+  feols(as.formula(paste(dependent_variable, "~", independent_variable_bin, "| occ2010 + ind1990 + race + sex + marst + educ + nchild + age")), 
+        data = analysis_data,
         weights = asecwth[asecwth > 0])
 }
 
@@ -138,12 +182,14 @@ for (diff_var in binary_moving_vars) {
 etable(any_baseline_models[[binary_moving_vars[1]]], any_indinfo_models[[binary_moving_vars[1]]],
        in_county_baseline_models[[binary_moving_vars[1]]], in_county_indinfo_models[[binary_moving_vars[1]]],
        out_county_baseline_models[[binary_moving_vars[1]]], out_county_indinfo_models[[binary_moving_vars[1]]],
-       vcov = "twoway", 
+       # vcov = "twoway",
+       # vcov = "standard",
+       vcov = ~serial,
+       # vcov = ~ind1990,
        headers = c("Moved - Baseline","Moved - Extended",
                    "Within County - Baseline","Within County - Extended",
                    "Out of County - Baseline","Out of County - Extended" 
                    ))
-
 
 ###########################
 ### Visual     Analysis ###
@@ -151,9 +197,9 @@ etable(any_baseline_models[[binary_moving_vars[1]]], any_indinfo_models[[binary_
 setwd(path_output)
 
 # Step 1: Extract Coefficients and Confidence Intervals
-all_results <- as.data.frame(c(bind_rows(any_baseline_models[[1]]$coeftable, any_indinfo_models[[1]]$coeftable,
-                              in_county_baseline_models[[1]]$coeftable, in_county_indinfo_models[[1]]$coeftable,
-                              out_county_baseline_models[[1]]$coeftable, out_county_indinfo_models[[1]]$coeftable)))
+all_results <- as.data.frame(c(bind_rows(any_baseline_models[[1]]$coeftable[1:2,], any_indinfo_models[[1]]$coeftable[1:2,],
+                              in_county_baseline_models[[1]]$coeftable[1:2,], in_county_indinfo_models[[1]]$coeftable[1:2,],
+                              out_county_baseline_models[[1]]$coeftable[1:2,], out_county_indinfo_models[[1]]$coeftable[1:2,])))
 all_results$household_type <- c("Partially Remote","Fully Remote",
                                 "Partially Remote","Fully Remote",
                                 "Partially Remote","Fully Remote",
